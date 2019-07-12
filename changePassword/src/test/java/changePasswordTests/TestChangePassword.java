fd8a28b3982a55cbf1319d7a492ac95264e4c231
Rahul Chakraborty <Rahul.Chakraborty2@cbre.com>
2019-07-12 12:14:40 +0530

    Initial Commit - Project Structure

---
+++ changePassword/src/test/java/changePasswordTests/TestChangePassword.java
@@ -0,0 +1,9 @@
+package changePasswordTests;
+import org.testng.annotations.Test;
+
+public class TestChangePassword {
+	@Test
+    public void TC01CheckValidPassword() {
+        System.out.println("This is just the structure now.");
+    }
+}
